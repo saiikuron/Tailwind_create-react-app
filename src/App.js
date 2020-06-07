ea2ffd5cbbefbbd7186330af0cf081980f82dfb0
saiikuron <52925758+saiikuron@users.noreply.github.com>
2020-06-07 12:03:51 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div className="App p-8 bg-gray-900 h-screen">
+      <p className="font-bold text-white text-4xl">Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
